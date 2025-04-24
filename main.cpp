e38f57e05e2bf9bda5c15527a9d57cde81ecce33
JannikOlmes <jannik.olmes@student.fh-kiel.de>
2025-04-24 23:59:03 +0200

    Vector vDishes hinzugefügt

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "PizzaMargherita.h"
 #include "VegetableSoup.h"
 #include "dishlib/AbstractDish.h"
+#include <memory>
+#include "DishFactory.h"
 
 using namespace std;
 using namespace dishlib;
 
 int main() {
+    vector<unique_ptr<AbstractDish>> vDishes;
+    vDishes.push_back(DishFactory::CreateDish(DishType::PizzaMargherita));
+    vDishes.push_back(DishFactory::CreateDish(DishType::VegetableSoup));
+
     PizzaMargherita pizza("Pizza Margherita");
     pizza.Prepare();
     std::cout << "------------------------" << std::endl;
