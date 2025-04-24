6b3f50c53c6ebb1d788659ce4b5e55872ec4c45a
lisa.skersies <lisa.skersies@student.fh-kiel.de>
2025-04-25 00:18:34 +0200

    Main neu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "VegetableSoup.h"
 #include "dishlib/AbstractDish.h"
 #include <memory>
+#include <algorithm>
 #include "DishFactory.h"
 
 using namespace std;
@@ -11,14 +12,24 @@ int main() {
     vector<unique_ptr<AbstractDish>> vDishes;
     std::unique_ptr<AbstractDish> pizza = DishFactory::CreateDish(DishType::PizzaMargherita);
     vDishes.push_back(std::move(pizza));
+    std::cout << "------------------------" << std::endl;
     std::unique_ptr<AbstractDish> soup = DishFactory::CreateDish(DishType::VegetableSoup);
     vDishes.push_back(std::move(soup));
 
-    PizzaMargherita pizza("Pizza Margherita");
-    pizza.Prepare();
-    std::cout << "------------------------" << std::endl;
-    VegetableSoup soup("Vegetable Soup");
-    soup.Prepare();
+    std::sort(vDishes.begin(), vDishes.end(),
+        [](const std::unique_ptr<AbstractDish>& a, const std::unique_ptr<AbstractDish>& b) {
+            return a->GetNumberIngredients() < b->GetNumberIngredients();
+        }
+    );
+
+    std::cout << "Sortierte Rezepte:\n";
+    for (const auto& dish : vDishes) {
+        std::cout << dish->GetDishName()
+                  << " (" << dish->GetNumberIngredients() << " Zutaten)"
+                  << std::endl;
+    }
+
+
 
     return 0;
 }
